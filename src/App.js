58cdcf797d467722abcbd8f46077e5c97bef31dc
Nikita Popov <Nikita.Popov@itechart-group.com>
2021-11-29 17:53:11 +0300

    New app component with movies

--- src/App.js
+++ src/App.js
@@ -2,42 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('constructor');
-  }
-
   state = {
-    count: 0
-  };
-
-  add = () => {
-    this.setState(state => ({count: state.count + 1}))
+    isLoading: true,
+    movies: []
   }
 
-  subtract = () => {
-    this.setState(state => ({count: state.count - 1}))
-  }
-
-  componentDidMount = () => {
-    console.log('componentDidMount');
-  }
-
-  componentDidUpdate = () => {
-    console.log('componentDidUpdate');
-  }
-
-  componentWillUnmount = () => {
-    console.log('componentWillUnmount');
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({isLoading: false})
+    }, 6000);
   }
 
   render() {
-    console.log('rendering');
-    return <div>
-      <h1>Current number: {this.state.count}</h1>
-      <button onClick={this.add}>Plus</button>
-      <button onClick={this.subtract}>Minus</button>
-    </div>  
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "Page is loaded"}</div>
   }
 }
 
