bd2ea453d582f5f9f9a346adeb89f14037161e12
Nikita Popov <Nikita.Popov@itechart-group.com>
2021-11-29 15:33:46 +0300

    Using props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Myname from './Myname'
+function Food({name}) {
+  return(    
+    <h3>I like to eat {name}!</h3>
+  );
+}
 
 function App() {
   return (
     <div>
       <h1>Hi there!</h1>
-      <Myname />
+      <Food name='borshch' />
+      <Food name='butter' />
+      <Food name='beaf' />
+      <Food name='pasta' />
     </div>
   );
 }
--- src/Myname.js
+++
@@ -1,7 +0,0 @@
-function Myname() {
-  return(
-    <h3>My name is Nick</h3>
-  );
-}
-
-export default Myname;
