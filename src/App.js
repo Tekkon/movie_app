55c7dc235d854fe1b375e77c0f2119882ff153fe
Nikita Popov <Nikita.Popov@itechart-group.com>
2021-11-29 16:46:21 +0300

    Using propTypes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
-function Food({name, picture}) {
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Food({name, picture, rating}) {
   return(    
     <div>
       <h3>I like {name}</h3>
+      <h4>{rating} / 5</h4>
       <img src={picture} alt="" />
     </div>
   );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
 }
 
 const foodILike = [
   {
     id: 1,
     name: 'borshch',
-    image: ''
+    image: '',
+    rating: 4.8
   },
   {
     id: 2,
     name: 'chease',
-    image: ''
+    image: '',
+    rating: 4.4
   },
   {
     id: 3,
     name: 'beaf',
-    image: ''
+    image: '',
+    rating: 4.3
   },
   {
     id: 4,
     name: 'pasta',
-    image: ''
+    image: '',
+    rating: 5.0
   }
 ]
 
 function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.image} />
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
 }
 
 function App() {
