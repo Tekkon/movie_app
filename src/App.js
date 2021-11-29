d0ad06f8b6fdddf8f0d9bb405c5ebda89a15f916
Nikita Popov <Nikita.Popov@itechart-group.com>
2021-11-29 19:53:40 +0300

    Movies appereance

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import Movie from './Movie';
+
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -9,16 +12,48 @@ class App extends React.Component {
   }
 
   getMovies = async () => {
-    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    const { 
+      data: { 
+        data: {movies} 
+      } 
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading: false});
   }
 
   componentDidMount() {
     this.getMovies();
   }
 
+  renderMovie(movie) {
+    console.log(movie);
+
+    return(
+      <div clas="movies">
+        <Movie 
+          key={movie.id}
+          id={movie.id} 
+          year={movie.year} 
+          title={movie.title} 
+          summary={movie.summary} 
+          poster={movie.medium_cover_image} 
+        />
+      </div>      
+    );
+  }
+
   render() {
-    const {isLoading} = this.state;
-    return <div>{isLoading ? "Loading..." : "Page is loaded"}</div>
+    const { isLoading, movies } = this.state;
+    return(
+      <section class="container">
+        {
+          isLoading 
+            ? <div class="loader">
+              <span class="locader__text">Loading...</span>
+            </div>
+            : movies.map(movie => this.renderMovie(movie))
+        }
+      </section>
+    );
   }
 }
 
