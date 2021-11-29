ef03a0354b10d4a9c233f7a8cab63eaea7b6ca96
Nikita Popov <Nikita.Popov@itechart-group.com>
2021-11-29 17:31:32 +0300

    component lifecycle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+
   state = {
     count: 0
   };
@@ -14,7 +19,20 @@ class App extends React.Component {
     this.setState(state => ({count: state.count - 1}))
   }
 
+  componentDidMount = () => {
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate = () => {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount = () => {
+    console.log('componentWillUnmount');
+  }
+
   render() {
+    console.log('rendering');
     return <div>
       <h1>Current number: {this.state.count}</h1>
       <button onClick={this.add}>Plus</button>
