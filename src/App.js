c63ed86e70a51010c6b736158416430cdc40351c
Nikita Popov <Nikita.Popov@itechart-group.com>
2021-11-30 15:39:46 +0300

    Home and about routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home'
+import About from './routes/About'
 
 function App() {
-  return <span>Something...</span>
+  return (
+    <HashRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>    
+    </HashRouter>
+  );
 }
 
 export default App;
