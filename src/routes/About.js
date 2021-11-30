028709be61fa0c9eafaf41e87712f62a66787fb6
Nikita Popov <Nikita.Popov@itechart-group.com>
2021-11-30 15:45:53 +0300

    Css for About and navigation

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import "./About.css";
 
 function About() {
-  return <span>About</span>;
+  return (
+    <div className="about__container">
+      <span>I made this poject because I really like movies!</span>
+    </div>
+  );
 }
 
 export default About;
