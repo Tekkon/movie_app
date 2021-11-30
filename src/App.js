e7b4e0a625c8193eac32ee2a4d2b61b93c78d314
Nikita Popov <Nikita.Popov@itechart-group.com>
2021-11-30 15:58:46 +0300

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import Home from './routes/Home'
 import About from './routes/About'
+
+import './App.css'
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
